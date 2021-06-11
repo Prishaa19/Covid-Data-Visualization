4fb22dd90f3c696d798caebfa875b1251a2ca75d
Prishaa Gupta <68915473+Prishaa19@users.noreply.github.com>
2021-06-11 21:28:11 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import plotly.express as px
+
+df = pd.read_csv("covidData.csv")
+
+fig = px.line(df, x="date", y="cases", color="country", title='Covid DataSheet')
+
+fig.show()
